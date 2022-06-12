ae9fbb41c54e3f02ccf332293c4dbcc195ca9190
Neeraj S D <neerajsd04@gmail.com>
2022-06-12 14:46:21 +0530

    working with props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <Person />
+        <Person name='Neeraj' age='20'></Person>
       </div>
     );
   }
