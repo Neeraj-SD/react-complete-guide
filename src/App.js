bcf0ad39b391a17dac9c63455ec6e9c1bdf04c0e
Neeraj S D <neerajsd04@gmail.com>
2022-06-12 16:19:41 +0530

    preferred way of conditional rendering in react

--- src/App.js
+++ src/App.js
@@ -50,19 +50,24 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies:Racing</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchPersonHandler} nameChanged={this.nameChangedHandler}></Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} ></Person>
+        </div>
+      )
+    }
 
 
     return (
       <div className="App" >
         <h1>Hi, I'm a React App.</h1>
         <button style={style} onClick={this.toggleShowPersons}>Switch</button>
-        {this.state.showPersons ?
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies:Racing</Person>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchPersonHandler} nameChanged={this.nameChangedHandler}></Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} ></Person>
-          </div> : null
-        }
+        {persons}
       </div>
     );
   }
